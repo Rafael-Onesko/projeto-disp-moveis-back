ca9e1d44f12006622c1b556f02c3278f58ca0aaa
rafael.onesko <rafael.onesko@mps.com.br>
2025-02-19 17:08:50 -0300

    chore

--- Cadastro/Model/UsuarioModel.cs
+++ Cadastro/Model/UsuarioModel.cs
@@ -1,7 +1,16 @@
-﻿namespace Cadastro.Model
+﻿using Cadastro.Domain;
+
+namespace Cadastro.Model
 {
     public class UsuarioModel
     {
+        List<RolesUsuario> RolesUsuario { get; set; }
+        List<Materias> MateriasUsuario { get; set; }
+        //public UsuarioModel()
+        //{
+        //    RolesUsuario = new List<RolesUsuario>();
+        //    MateriasUsuario = new List<Materias>(); 
+        //}
         public string Email { get; set; }
         public string PrimeiroNome { get; set; }
         public string UltimoNome { get; set; }
